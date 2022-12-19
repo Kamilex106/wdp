c86973e92379121bddce5ae08df7d00248aa4241
Kamilex106 <115708858+Kamilex106@users.noreply.github.com>
2022-12-19 11:10:50 +0100

    Add files via upload

---
+++ Kamil_Lel5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int nwd(int a, int b);
+int main()
+{
+    int liczba;
+    int liczba2;
+    cout << "Podaj 1 liczbe: ";
+    cin >> liczba;
+    cout << "Podaj 2 liczbe: ";
+    cin >> liczba2;
+    cout << nwd(liczba, liczba2);
+}
+
+int nwd(int a, int b) {
+    int reszta;
+    reszta = (a % b);
+    if (a % b == 0) {
+        return b;
+    }
+    a = int b;
+    int b = reszta;
+
+
+}
